ac7391a2413ebff661f757a4e93ba14d481c5909
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-06-08 00:56:14 +0100

    Timer tests

--- tests/components/Timer.test.jsx
+++ tests/components/Timer.test.jsx
@@ -9,4 +9,47 @@ describe ('<Timer />', () => {
   it ('should exist', () => {
     expect(Timer).toExist();
   });
+
+  it ('should start off paused with a count of 0', () => {
+    let timer = TestUtils.renderIntoDocument(<Timer />);
+    expect(timer.state.count).toBe(0);
+    expect(timer.state.countdownStatus).toBe('paused');
+  });
+
+  it ('should count upwards when started', () => {
+    let timer = TestUtils.renderIntoDocument(<Timer />);
+    expect(timer.state.count).toBe(0);
+    timer.handleStatusChange('started');
+    setTimeout(() => {
+      expect(timer.state.count).toBe(1);
+      done();
+    }, 1001);
+  });
+
+  it ('should stop counting when paused', () => {
+    let timer = TestUtils.renderIntoDocument(<Timer />);
+    expect(timer.state.count).toBe(0);
+    timer.handleStatusChange('started');
+    setTimeout(() => {
+      expect(timer.state.count).toBe(1);
+      timer.handleStatusChange('pause');
+      done();
+    }, 1001);
+    setTimeout(() => {
+      expect(timer.state.count).toBe(1);
+      done();
+    }, 1001);
+  });
+
+  it ('when in the started state should reset count to 0 and to paused when cancel is pressed', () => {
+    let timer = TestUtils.renderIntoDocument(<Timer />);
+    timer.handleStatusChange('started');
+    setTimeout(() => {
+      expect(timer.state.count).toBe(1);
+      done();
+    }, 1001);
+    timer.handleStatusChange('stopped');
+    expect(timer.state.count).toBe(0);
+    expect(timer.state.countdownStatus).toBe('paused');
+  });
 });
